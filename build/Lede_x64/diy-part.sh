080c6829ce321dd54b0ed562b644dc07be6022b3
ywz318 <46588676+ywz318@users.noreply.github.com>
2021-12-24 22:00:49 +0800

    Update diy-part.sh

--- build/Lede_x64/diy-part.sh
+++ build/Lede_x64/diy-part.sh
@@ -7,10 +7,9 @@
 # 如果你的OP是当主路由的话，网关、DNS、广播都不需要，代码前面加 # 注释掉，只保留后台地址和子网掩码就可以
 # 如果你有编译ipv6的话，‘去掉LAN口使用内置的 IPv6 管理’代码前面也加 # 注释掉
 
-src-git helloworld https://github.com/fw876/helloworld
-src-git luci-app-aliddns https://github.com/honwen/luci-app-aliddns.git
-src-git luci-app-poweroff https://github.com/esirplayground/luci-app-poweroff
-src-git luci-app-autotimeset https://github.com/sirpdboy/luci-app-autotimeset
+
+git clone https://github.com/fw876/helloworld package/luci-app-ssr-plus
+git clone https://github.com/sirpdboy/luci-app-autotimeset package/autotimeset
 
 
 cat >$NETIP <<-EOF
